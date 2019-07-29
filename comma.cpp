fa5eb33f8bf85035d530e88a7c693ad71c277092
yinizhizhu <lijiahe19941013@sina.cn>
2019-07-29 09:58:06 +0800

    For comma

---
+++ comma.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int x = 0, y = 0, z = 0;
+	cout << x << ", " << y << " " << z << endl;
+	x, y, z = 2 * 2, 3 * 3, 4 * 4;
+	cout << x << ", " << y << ", " << z << endl;
+	return 0;
+}
